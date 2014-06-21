7b7929c71318cfab2f8c3fcd2cb0979bd5f09789
awefcxsd <m80102502@gmail.com>
2014-06-22 05:17:06 +0800

    add arrange list

--- Do-It-Today/src/com/loulijun/demo2/ArrangeSlidePageFragment.java
+++ Do-It-Today/src/com/loulijun/demo2/ArrangeSlidePageFragment.java
@@ -3,36 +3,78 @@ package com.loulijun.demo2;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ArrangeSlidePageFragment extends Fragment {
-	String data;
-	
+	String date;
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	}
-	
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+
 		ViewGroup rootView = (ViewGroup) inflater.inflate(
 				R.layout.arrangefragment, container, false);
-		TextView output = (TextView) rootView.findViewById(R.id.textViewFragment);
+		TextView output = (TextView) rootView
+				.findViewById(R.id.textViewFragment);
 		Bundle Data = this.getArguments();
-		
-		int position=Data.getInt("pos");
+
+		int position = Data.getInt("pos");
 		Calendar current = new GregorianCalendar();
-    	current.add(Calendar.DATE, position-500);
-    	
-		String date=current.get(Calendar.YEAR)+"/"+(current.get(Calendar.MONTH)+1)+"/"+current.get(Calendar.DATE);
+		current.add(Calendar.DATE, position - 500);
+
+		date = current.get(Calendar.YEAR) + "/"
+				+ (current.get(Calendar.MONTH) + 1) + "/"
+				+ current.get(Calendar.DATE);
 		output.setText(date);
+
+		listView = (ListView) rootView.findViewById(R.id.DateEventList);
+
+		adapter = new ArrayAdapter<String>(this.getActivity(),
+				android.R.layout.simple_list_item_1);
+		for (int i = 0; i < 24; i++) {
+			adapter.add(i + ":00");
+		}
+
+		listView.setAdapter(adapter);
+
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+	
+			}
+
+		});
+
 		return rootView;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- Do-It-Today/src/com/loulijun/demo2/ListActivity.java
+++ Do-It-Today/src/com/loulijun/demo2/ListActivity.java
@@ -2,16 +2,57 @@ package com.loulijun.demo2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loulijun.demo2.data.*;
 
 public class ListActivity extends Activity {
 
+	private ListView listView;
+
+	private ArrayAdapter<String> adapter;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
-		TextView textView=new TextView(this);
-		textView.setText("6");
-		setContentView(textView);
+
+		setContentView(R.layout.list);
+
+		listView = (ListView) findViewById(R.id.EventList);
+
+		// ²M³æ°}¦C
+
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1);
+		adapter.add("¬õ¨§");
+		adapter.add("¶Â¨§");
+		adapter.add("ºñ¨§");
+		adapter.add("ªá¨§");
+		adapter.add("¤ò¨§");
+		adapter.add("¤g¨§");
+		adapter.add("¨¡ÀY");
+		adapter.add("¦a¥Ê");
+
+		listView.setAdapter(adapter);
+
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				ListView listView = (ListView) parent;
+				Toast.makeText(ListActivity.this, "ID¡G" + position + "HEY",
+						Toast.LENGTH_LONG).show();
+			}
+
+		});
+
 	}
-	
+
 }
