20c6c4d00ebefb8fdecdf7613ef0e04398212e85
momentchan <Fred@Fred-PC>
2014-06-21 21:24:25 +0800

    initial

---
+++ Do-It-Today/src/com/flysnow/sina/weibo/HomeActivity.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.flysnow.sina.weibo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * 擐△Activity
+ * @author 憌��
+ * @since 2011-3-8
+ */
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		TextView textView=new TextView(this);
+		textView.setText("2");
+		setContentView(textView);
+	}
+	
+}
