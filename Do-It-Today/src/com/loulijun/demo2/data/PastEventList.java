91a69bf2f88d1f316082ffef194c831d6ce0f7f9
awefcxsd <m80102502@gmail.com>
2014-06-24 23:00:58 +0800

    past file save read

--- Do-It-Today/src/com/loulijun/demo2/data/PastEventList.java
+++ Do-It-Today/src/com/loulijun/demo2/data/PastEventList.java
@@ -1,16 +1,25 @@
 package com.loulijun.demo2.data;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
 import android.util.Log;
 
 public class PastEventList implements Serializable {
-	Map<String,CalDay> map=new HashMap<String,CalDay>();
+	public Map<String,CalDay> map=new HashMap<String,CalDay>();
 	
 	public PastEventList() {
 		// TODO Auto-generated constructor stub
@@ -25,5 +34,59 @@ public class PastEventList implements Serializable {
 		map.put(key, dayEvent);
 	}
 	
+	public void saveToFile(Service runing) {
+		FileOutputStream fout = null;
+		ObjectOutputStream oos = null;
+		try {
+			fout = runing.openFileOutput("past", Context.MODE_PRIVATE);
+			oos = new ObjectOutputStream(fout);
+			oos.writeObject(map);
+		} catch (Exception e) {
 
+		} finally {
+			if (fout != null) {
+				try {
+					fout.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+			if (oos != null) {
+				try {
+					oos.close();
+				} catch (IOException e2) {
+					e2.printStackTrace();
+				}
+			}
+		}
+	}
+
+	
+	public void readFromFile(Activity runing) {
+		FileInputStream fin = null;
+		ObjectInputStream ois = null;
+		try {
+			fin = runing.openFileInput("past");
+			ois = new ObjectInputStream(fin);
+			if(fin != null && ois != null)
+			map = (Map<String, CalDay>) ois.readObject();
+		} catch (Exception e) {
+
+		} finally {
+			if (fin != null) {
+				try {
+					fin.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+			if (ois != null) {
+				try {
+					ois.close();
+				} catch (IOException e2) {
+					e2.printStackTrace();
+				}
+			}
+		}
+	}
 }
