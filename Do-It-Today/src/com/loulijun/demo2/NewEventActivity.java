321a2589bb63979907a3c5ae6191bea5ec12c97f
momentchan <Fred@Fred-PC>
2014-06-26 00:04:53 +0800

    new event

--- Do-It-Today/src/com/loulijun/demo2/NewEventActivity.java
+++ Do-It-Today/src/com/loulijun/demo2/NewEventActivity.java
@@ -1,5 +1,6 @@
 package com.loulijun.demo2;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -49,6 +50,10 @@ public class NewEventActivity extends ActionBarActivity {
 	int minute;
 	private ResponseReceiver receiver;
 	final Context context = this;
+	Calendar deadline = new GregorianCalendar();
+	SimpleDateFormat sdf = new SimpleDateFormat("MMMd, yyyy");
+	SimpleDateFormat sdf2 = new SimpleDateFormat("hh:mm aaa");
+	
 	
 	public void setProgressBarColor(SeekBar progressBar, int newColor){ 
 	    LayerDrawable ld = (LayerDrawable) progressBar.getProgressDrawable();
@@ -62,48 +67,90 @@ public class NewEventActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add);
 		Log.d("", "on");
-	
-		/*
-		EditText startday = (EditText) findViewById(R.id.editText4);
-		startday.setOnClickListener(new View.OnClickListener() {
+		Typeface type = Typeface.createFromAsset(getAssets(),"fonts/monofonto.ttf");
+		
+		//Daypicker Setting
+		Calendar c = Calendar.getInstance();
+		String strDate = sdf.format(c.getTime());
+		EditText end_day = (EditText) findViewById(R.id.editText4);
+		end_day.setKeyListener(null);
+		end_day.setText(strDate);
+		end_day.setTypeface(type);
+		end_day.setTextColor(Color.GRAY);
+		end_day.setTextSize(20);
+		end_day.setOnClickListener(new View.OnClickListener() {
 			
 	        @Override
 	        public void onClick(View v) {
 	        	LayoutInflater inflater = getLayoutInflater();
 	    		final View layout = inflater.inflate( R.layout.daypick,
 	    				(ViewGroup) findViewById(R.id.start));
-	    		TextView text = (TextView) layout.findViewById(R.id.textView1);
-	    		text.setTextSize(25);
-	    		text.setText(title.getText().toString()+ " has been added to the list.");
-	    		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+	    		AlertDialog.Builder dialog = new AlertDialog.Builder(NewEventActivity.this);
 	    		dialog.setView(layout);
-	    		dialog.setTitle("Succeeded"); // 設定dialog 的title顯示內容
 	    		dialog.setCancelable(false);
+	    		dialog.setTitle("Choose Day");
 	    		dialog.setPositiveButton("OK",
 	    				new DialogInterface.OnClickListener() {
 	    					@Override
 	    					public void onClick(DialogInterface dialog, int which) {
+	    						DatePicker date = (DatePicker) layout.findViewById(R.id.datePicker2);
+	    						deadline.set(date.getYear(), date.getMonth(), date.getDayOfMonth(),0,0);
+	    						EditText end_day = (EditText) findViewById(R.id.editText4);
+	    						String strDate = sdf.format(deadline.getTime());
+	    						end_day.setText(strDate);
 	    					}});
 	    		dialog.show();
 	        }
 
-	    });*/
+	    });
 		
 		
+		String strTime = sdf2.format(c.getTime());
+		EditText end_time = (EditText) findViewById(R.id.editText5);
+		end_time.setKeyListener(null);
+		end_time.setText(strTime);
+		end_time.setTypeface(type);
+		end_time.setTextColor(Color.GRAY);
+		end_time.setTextSize(20);
+		end_time.setOnClickListener(new View.OnClickListener() {
+			
+	        @Override
+	        public void onClick(View v) {
+	        	LayoutInflater inflater = getLayoutInflater();
+	    		final View layout = inflater.inflate( R.layout.timepicker,
+	    				(ViewGroup) findViewById(R.id.start));
+	    	
+	    		TimePicker time = (TimePicker) layout.findViewById(R.id.timePicker1);
+				time.setIs24HourView(false);
+				time.setOnTimeChangedListener(new OnTimeChangedListener() {
+					@Override
+					public void onTimeChanged(TimePicker arg0, int arg1, int arg2) {
+						// 小時 ,分鐘
+						hour=arg1;
+						minute=arg2;
+					}
+				});
+	    		
+	    		AlertDialog.Builder dialog = new AlertDialog.Builder(NewEventActivity.this);
+	    		dialog.setView(layout);
+	    		dialog.setCancelable(false);
+	    		dialog.setTitle("Choose Time");
+	    		dialog.setPositiveButton("OK",
+	    				new DialogInterface.OnClickListener() {
+	    					@Override
+	    					public void onClick(DialogInterface dialog, int which) {
+	    						EditText end_time = (EditText) findViewById(R.id.editText5);
+	    						if(hour>11&&hour!=24)
+	    							end_time.setText(String.valueOf(hour-12)+":"+String.valueOf(minute)+ "PM");
+	    						else
+	    							end_time.setText(String.valueOf(hour)+":"+String.valueOf(minute)+ "AM");
+	    					}});
+	    		dialog.show();
+	        }
+
+	    });
 		
-		//TimePicker Setting 
-		TimePicker time = (TimePicker) findViewById(R.id.timePicker1);
-		time.setIs24HourView(true);
-		time.setOnTimeChangedListener(new OnTimeChangedListener() {
-			@Override
-			public void onTimeChanged(TimePicker arg0, int arg1, int arg2) {
-				// 小時 ,分鐘
-				hour=arg1;
-				minute=arg2;
-			}
-		});
-		
-		
+				
 		//SeekBar Setting
 		SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar1);
 		seekBar.setProgress(0);
@@ -144,7 +191,7 @@ public class NewEventActivity extends ActionBarActivity {
 
 		GlobalV global = ((GlobalV) getApplicationContext());
 		global.flexList.list.size();
-		DatePicker date = (DatePicker) findViewById(R.id.datePicker1);
+		//DatePicker date = (DatePicker) findViewById(R.id.datePicker1);
 		TimePicker time = (TimePicker) findViewById(R.id.timePicker1);
 		EditText title = (EditText) findViewById(R.id.editText1);
 		EditText description = (EditText) findViewById(R.id.editText2);
@@ -169,8 +216,8 @@ public class NewEventActivity extends ActionBarActivity {
 		dialog.show();
 				
 		
-		Calendar deadline = new GregorianCalendar();
-		deadline.set(date.getYear(), date.getMonth(), date.getDayOfMonth(),hour,minute);
+		deadline.set(deadline.get(Calendar.YEAR), deadline.get(Calendar.MONTH), deadline.get(Calendar.DATE),hour,minute);
+		
 
 		SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar1);
 
